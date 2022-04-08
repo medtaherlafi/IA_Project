da47395857f8c4d11067e5240fb4df8b41ec8820
medtaherlafi <mohamed-tahar.lafi@etu.u-paris.fr>
2022-04-09 00:22:20 +0200

    implémentation du jeu

--- jeu/constants.py
+++ jeu/constants.py
@@ -3,12 +3,21 @@ import pygame
 
 screen = get_monitors()[0]
 FPS = 60
-HEIGHT,WIDTH=screen.height - 70, screen.width
+HEIGHT,WIDTH=800, 800
+#HEIGHT,WIDTH=screen.height - 70, screen.width  #j'ai besoin de modifier ça un peu ça marche pas pour le moment 
+
+#Nombre de Lignes et de Colonnes
 ROWS, COLS = 8, 8
+
+#Longueur de la case
 SQUARE_SIZE=WIDTH//COLS
+
+#Représentation des couleurs
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GREY = (128,128,128)
+
+#La couronne quand une pièce atteint le territoire de l'adversaire ?
 COURONNE= pygame.transform.scale(pygame.image.load('divers/couronne.png'), (44, 25))
--- jeu/menu.py
+++ jeu/menu.py
@@ -1,6 +1,11 @@
 import pygame
 import pygame_menu
 from constants import *
+from game import Game
+from IA.algorithm import minimax
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Jeu de Dames')
 
 def set_difficulty(value, difficulty):
     pass
@@ -11,10 +16,59 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
+def jouerADeux():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+    
+    pygame.quit()
 
 def start_the_game():
-    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption('Jeu de Dames')
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+        clock.tick(FPS)
+        
+        if game.turn == WHITE:
+            value, new_board = minimax(game.get_board(), 4, WHITE, game)
+            game.ai_move(new_board)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+    
+    pygame.quit()
 
 def set_menu():
     """Création du menu du jeu. On peut choisir le niveau de difficulté, si on veut commencer à jouer ou si on veut quitter le jeu"""
@@ -23,12 +77,15 @@ def set_menu():
     surface = pygame.display.set_mode((WIDTH, HEIGHT))
     menu = pygame_menu.Menu('Bienvenue', WIDTH, HEIGHT,
                             theme=pygame_menu.themes.THEME_DARK)
+        
+    #Jouer à deux
+    menu.add.button('2 joueurs', jouerADeux)
     
     #Paramètrage des Difficultés
     menu.add.selector('Difficulté :', [('Facile', 1),  ('Intermédiaire', 2), ('Expert', 3),], onchange=set_difficulty)
     
-    #Jouer
-    menu.add.button('Jouer', start_the_game)
+    #Jouer contre une IA
+    menu.add.button('1 joueur VS I.A', start_the_game)
     
     #Quitter
     menu.add.button('Quitter', pygame_menu.events.EXIT)
